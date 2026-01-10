629fb8f911d7c39544f684929838777cd032fdd3
Roland Chastain <roland.chastain@gmx.fr>
2026-01-10 12:34:15 +0100

    Add parameters

--- pascal/cardioide/mageia/conchoid1.pas
+++ pascal/cardioide/mageia/conchoid1.pas
@@ -16,11 +16,12 @@ const
   Y = 0;
   
 var
-  context, context2: pcairo_t;
-  surface, surface2: pcairo_surface_t;
+  cr1, cr2: pcairo_t;
+  sf1, sf2: pcairo_surface_t;
   a,
   xx, yy,
   rr: double;
+  sw, sh: integer;
 
 const
   D = 0.3;
@@ -33,12 +34,15 @@ var
   filename: string;
 
 begin
+  sw := StrToIntDef(ParamStr(1), SURFACE_WIDTH);
+  sh := StrToIntDef(ParamStr(2), SURFACE_HEIGHT);
+  
   darkblue  := TCairoColor.Create($262F45);
   lightblue := TCairoColor.Create($2397D4);
   white     := TCairoColor.Create($FFFFFF);
 { https://wiki.mageia.org/en/Directives_pour_la_conception_graphique-fr#Palette_de_couleurs_officielle }
   
-  if ParamStr(1) = '--dark' then
+  if (ParamStr(1) = '--dark') or (ParamStr(3) = '--dark') then
   begin
     bkcolor := darkblue;
     linecolor := lightblue;
@@ -50,38 +54,38 @@ begin
     filename := 'light';
   end;
   pointcolor := white;
-  filename := Format(Concat(filename, '-%dx%d.png'), [SURFACE_WIDTH, SURFACE_HEIGHT]);
+  filename := Format(Concat(filename, '-%dx%d.png'), [sw, sh]);
   
-  surface := cairo_image_surface_create(CAIRO_FORMAT_ARGB32, SURFACE_WIDTH, SURFACE_HEIGHT);
-  context := cairo_create(surface);
+  sf1 := cairo_image_surface_create(CAIRO_FORMAT_ARGB32, sw, sh);
+  cr1 := cairo_create(sf1);
   
-  surface2 := cairo_image_surface_create(CAIRO_FORMAT_ARGB32, SURFACE_WIDTH, SURFACE_HEIGHT);
-  context2 := cairo_create(surface2);
+  sf2 := cairo_image_surface_create(CAIRO_FORMAT_ARGB32, sw, sh);
+  cr2 := cairo_create(sf2);
   
-  cairo_save(context);
+  cairo_save(cr1);
   
-  cairo_scale(context, SURFACE_HEIGHT, SURFACE_HEIGHT);
-  cairo_translate(context, (1 / 2) * (SURFACE_WIDTH / SURFACE_HEIGHT), 1 / 2);
-  cairo_scale(context2, SURFACE_HEIGHT, SURFACE_HEIGHT);
-  cairo_translate(context2, (1 / 2) * (SURFACE_WIDTH / SURFACE_HEIGHT), 1 / 2);
+  cairo_scale(cr1, sh, sh);
+  cairo_translate(cr1, (1 / 2) * (sw / sh), 1 / 2);
+  cairo_scale(cr2, sh, sh);
+  cairo_translate(cr2, (1 / 2) * (sw / sh), 1 / 2);
   
-  cairo_set_line_width(context, 1 / 500);
-  cairo_set_line_cap(context, CAIRO_LINE_CAP_ROUND);
-  cairo_set_line_join(context, CAIRO_LINE_JOIN_ROUND);
+  cairo_set_line_width(cr1, 1 / 500);
+  cairo_set_line_cap(cr1, CAIRO_LINE_CAP_ROUND);
+  cairo_set_line_join(cr1, CAIRO_LINE_JOIN_ROUND);
   
-  with bkcolor do cairo_set_source_rgb(context, r, g, b);
-  cairo_paint(context);
+  with bkcolor do cairo_set_source_rgb(cr1, r, g, b);
+  cairo_paint(cr1);
   
- {cairo_set_source_rgb(context, 0, 0, 1);
-  cairo_arc(context, 0, 0, R, 0, 2 * PI);
-  cairo_stroke(context);
+ {cairo_set_source_rgb(cr1, 0, 0, 1);
+  cairo_arc(cr1, 0, 0, R, 0, 2 * PI);
+  cairo_stroke(cr1);
   
-  cairo_set_source_rgb(context, 0, 0, 0);
-  cairo_arc(context, X, Y, R2, 0, 2 * PI);
-  cairo_fill(context);}
+  cairo_set_source_rgb(cr1, 0, 0, 0);
+  cairo_arc(cr1, X, Y, R2, 0, 2 * PI);
+  cairo_fill(cr1);}
   
-  with linecolor do cairo_set_source_rgb(context, r, g, b);
-  with pointcolor do cairo_set_source_rgb(context2, r, g, b);
+  with linecolor do cairo_set_source_rgb(cr1, r, g, b);
+  with pointcolor do cairo_set_source_rgb(cr2, r, g, b);
   
   a := PI / 36;
   
@@ -99,26 +103,26 @@ begin
     xx2 := xx + (D / rr) * dx;
     yy2 := yy + (D / rr) * dy;
     
-    cairo_move_to(context, xx1, yy1);
-    cairo_line_to(context, xx2, yy2);
-    cairo_stroke(context);
+    cairo_move_to(cr1, xx1, yy1);
+    cairo_line_to(cr1, xx2, yy2);
+    cairo_stroke(cr1);
     
-    cairo_arc(context2, xx, yy, R2, 0, 2 * PI);
-    cairo_arc(context2, xx1, yy1, R2, 0, 2 * PI);
-    cairo_arc(context2, xx2, yy2, R2, 0, 2 * PI);
-    cairo_fill(context2);
+    cairo_arc(cr2, xx, yy, R2, 0, 2 * PI);
+    cairo_arc(cr2, xx1, yy1, R2, 0, 2 * PI);
+    cairo_arc(cr2, xx2, yy2, R2, 0, 2 * PI);
+    cairo_fill(cr2);
     
     a := a + PI / 18;
   end;
   
-  cairo_restore(context);
-  cairo_set_source_surface(context, surface2, 0.0, 0.0);
-  cairo_paint(context);
+  cairo_restore(cr1);
+  cairo_set_source_surface(cr1, sf2, 0.0, 0.0);
+  cairo_paint(cr1);
   
-  cairo_surface_write_to_png(surface, pchar(filename));
+  cairo_surface_write_to_png(sf1, pchar(filename));
 
-  cairo_destroy(context);
-  cairo_surface_destroy(surface);
-  cairo_destroy(context2);
-  cairo_surface_destroy(surface2);
+  cairo_destroy(cr1);
+  cairo_surface_destroy(sf1);
+  cairo_destroy(cr2);
+  cairo_surface_destroy(sf2);
 end.
