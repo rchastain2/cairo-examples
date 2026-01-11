93707b4f231c0086a7f18c0c795a9f2b64625408
Roland Chastain <roland.chastain@gmx.fr>
2026-01-11 13:52:30 +0100

    Add PTCPas example

---
+++ pascal/ptc/cardioid1.pas
@@ -0,0 +1,117 @@
+
+{ 
+  Animation illustrating a method for drawing a cardioid
+  https://mathimages.swarthmore.edu/index.php/Cardioid
+}
+
+program Cardioid1;
+
+{$MODE objfpc}
+
+uses
+  SysUtils, ptc, Cairo;
+
+const
+  R = 0.15;
+  X = -R;
+  Y = 0;
+var
+  xx, yy, rr: double;
+const
+  D = 0.3;
+var
+  dx, dy: double;
+  xx1, yy1, xx2, yy2: double;
+const
+  CONSOLE_W = 512;
+  CONSOLE_H = CONSOLE_W;
+  DELAY = 60;
+var
+  console: IPTCConsole;
+  format: IPTCFormat;
+  surface: IPTCSurface;
+  width, height: Integer;
+  pixels: PUint32;
+  sf: pcairo_surface_t;
+  cr: pcairo_t;
+  ang: double;
+  
+begin
+  try
+    console := TPTCConsoleFactory.CreateNew;
+    format := TPTCFormatFactory.CreateNew(32, $00FF0000, $0000FF00, $000000FF);
+    console.open('A method for drawing a cardioid (PTCPas & Cairo example)', CONSOLE_W, CONSOLE_H, format);
+    surface := TPTCSurfaceFactory.CreateNew(console.width, console.height, format);
+    
+    ang := PI / 72;
+    
+    while not console.KeyPressed do
+    begin
+      width := surface.width;
+      height := surface.height;
+      
+      pixels := surface.lock;
+      sf := cairo_image_surface_create_for_data(pbyte(pixels), CAIRO_FORMAT_ARGB32, width, height, cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, width));
+      cr := cairo_create(sf);
+      
+      cairo_scale(cr, width, height);
+      cairo_translate(cr, 1 / 2, 1 / 2);
+      cairo_set_line_width(cr, 1 / 400);
+      
+      cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
+      cairo_paint(cr);
+      
+      cairo_set_source_rgb(cr, 0, 0, 1);
+      cairo_arc(cr, 0, 0, R, 0, 2 * PI);
+      cairo_stroke(cr);
+      
+      cairo_set_source_rgb(cr, 0, 0, 0);
+      cairo_arc(cr, X, Y, 1 / 150, 0, 2 * PI);
+      cairo_fill(cr);
+      
+      xx := R * Cos(ang);
+      yy := R * Sin(ang);
+      
+      dx := xx - X;
+      dy := yy - Y;
+      
+      rr := Sqrt(Sqr(dx) + Sqr(dy));
+      
+      xx1 := xx - (D / rr) * dx;
+      yy1 := yy - (D / rr) * dy;
+      xx2 := xx + (D / rr) * dx;
+      yy2 := yy + (D / rr) * dy;
+      
+      cairo_set_source_rgba(cr, 1, 0, 0, 0.5);
+      cairo_move_to(cr, xx1, yy1);
+      cairo_line_to(cr, xx2, yy2);
+      cairo_stroke(cr);
+      
+      cairo_set_source_rgb(cr, 0, 0, 0);
+      cairo_arc(cr, xx, yy, 1 / 150, 0, 2 * PI);
+      cairo_arc(cr, xx1, yy1, 1 / 150, 0, 2 * PI);
+      cairo_arc(cr, xx2, yy2, 1 / 150, 0, 2 * PI);
+      cairo_fill(cr);
+      
+      cairo_destroy(cr);
+      cairo_surface_destroy(sf);
+      
+      surface.unlock;
+      surface.copy(console);
+      console.update;
+      
+      ang := ang + PI / 36;
+      if ang > 2 * PI then
+        ang := ang - 2 * PI;
+      
+      Sleep(DELAY);
+    end;
+    
+    if Assigned(console) then
+      console.close;
+    
+  except
+    on error: TPTCError do
+      error.report;
+  end;
+end.
--- pascal/ptcgraph/cardioid1.pas
+++ pascal/ptcgraph/cardioid1.pas
@@ -10,7 +10,6 @@ uses
 {$IFDEF unix}
   cThreads,
 {$ENDIF}
-  //SysUtils,
   ptcCrt,
   ptcGraph,
   Cairo;
@@ -48,15 +47,15 @@ const
   X = -R;
   Y = 0;
 var
-  ctx: pcairo_t;
+  cr: pcairo_t;
   a, xx, yy: double;
 const
   D = 0.3;
 begin
   result := cairo_image_surface_create(CAIRO_FORMAT_ARGB32, AWidth, AHeight);
-  ctx := cairo_create(result);
-  cairo_scale(ctx, AWidth, AHeight);
-  cairo_translate(ctx, 1 / 2, 1 / 2);
+  cr := cairo_create(result);
+  cairo_scale(cr, AWidth, AHeight);
+  cairo_translate(cr, 1 / 2, 1 / 2);
   
   a := 0;
   while a < 2 * PI do
@@ -65,24 +64,24 @@ begin
     yy := 2 * R * Sin(a) * (1 + Cos(a)) + Y;
     
     if a = 0 then
-      cairo_move_to(ctx, xx, yy)
+      cairo_move_to(cr, xx, yy)
     else
-      cairo_line_to(ctx, xx, yy);
+      cairo_line_to(cr, xx, yy);
     
     a := a + PI / 49;
   end;
   
-  cairo_set_line_width(ctx, 1 / 300);
-  cairo_set_source_rgb(ctx, 1, 0, 0);
-  cairo_stroke(ctx);
-  cairo_destroy(ctx);
+  cairo_set_line_width(cr, 1 / 300);
+  cairo_set_source_rgb(cr, 1, 0, 0);
+  cairo_stroke(cr);
+  cairo_destroy(cr);
 end;
 
-procedure CairoDraw(var AImage: TImage; const ABackground: pcairo_surface_t; const AAngle: double);
+procedure CairoDraw(var AImage: TImage; const AStaticBackground: pcairo_surface_t; const AAngle: double);
 var
-  sfc: pcairo_surface_t;
-  ctx: pcairo_t;
-  str: integer;
+  sf: pcairo_surface_t;
+  cr: pcairo_t;
+  stride: integer;
 const
   R = 0.15;
   X = -R;
@@ -95,28 +94,28 @@ var
   dx, dy: double;
   xx1, yy1, xx2, yy2: double;
 begin
-  str := cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, AImage.Header.Width);
-  sfc := cairo_image_surface_create_for_data(@AImage.Data[0], CAIRO_FORMAT_ARGB32, AImage.Header.Width, AImage.Header.Height, str);
-  ctx := cairo_create(sfc);
+  stride := cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, AImage.Header.Width);
+  sf := cairo_image_surface_create_for_data(@AImage.Data[0], CAIRO_FORMAT_ARGB32, AImage.Header.Width, AImage.Header.Height, stride);
+  cr := cairo_create(sf);
   
-  cairo_set_source_rgb(ctx, 1.0, 1.0, 1.0);
-  cairo_paint(ctx);
+  cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
+  cairo_paint(cr);
   
-  cairo_set_source_surface(ctx, ABackground, 0, 0);
-  cairo_paint(ctx);
+  cairo_set_source_surface(cr, AStaticBackground, 0, 0);
+  cairo_paint(cr);
   
-  cairo_scale(ctx, AImage.Header.Width, AImage.Header.Height);
-  cairo_translate(ctx, 0.5, 0.5);
+  cairo_scale(cr, AImage.Header.Width, AImage.Header.Height);
+  cairo_translate(cr, 0.5, 0.5);
   
-  cairo_set_line_width(ctx, 1 / 400);
+  cairo_set_line_width(cr, 1 / 400);
   
-  cairo_set_source_rgb(ctx, 0, 0, 1);
-  cairo_arc(ctx, 0, 0, R, 0, 2 * PI);
-  cairo_stroke(ctx);
+  cairo_set_source_rgb(cr, 0, 0, 1);
+  cairo_arc(cr, 0, 0, R, 0, 2 * PI);
+  cairo_stroke(cr);
   
-  cairo_set_source_rgb(ctx, 0, 0, 0);
-  cairo_arc(ctx, X, Y, 1 / 150, 0, 2 * PI);
-  cairo_fill(ctx);
+  cairo_set_source_rgb(cr, 0, 0, 0);
+  cairo_arc(cr, X, Y, 1 / 150, 0, 2 * PI);
+  cairo_fill(cr);
   
   xx := R * Cos(AAngle);
   yy := R * Sin(AAngle);
@@ -131,19 +130,19 @@ begin
   xx2 := xx + (D / rr) * dx;
   yy2 := yy + (D / rr) * dy;
   
-  cairo_set_source_rgba(ctx, 1, 0, 0, 0.5);
-  cairo_move_to(ctx, xx1, yy1);
-  cairo_line_to(ctx, xx2, yy2);
-  cairo_stroke(ctx);
+  cairo_set_source_rgba(cr, 1, 0, 0, 0.5);
+  cairo_move_to(cr, xx1, yy1);
+  cairo_line_to(cr, xx2, yy2);
+  cairo_stroke(cr);
   
-  cairo_set_source_rgb(ctx, 0, 0, 0);
-  cairo_arc(ctx, xx, yy, 1 / 150, 0, 2 * PI);
-  cairo_arc(ctx, xx1, yy1, 1 / 150, 0, 2 * PI);
-  cairo_arc(ctx, xx2, yy2, 1 / 150, 0, 2 * PI);
-  cairo_fill(ctx);
+  cairo_set_source_rgb(cr, 0, 0, 0);
+  cairo_arc(cr, xx, yy, 1 / 150, 0, 2 * PI);
+  cairo_arc(cr, xx1, yy1, 1 / 150, 0, 2 * PI);
+  cairo_arc(cr, xx2, yy2, 1 / 150, 0, 2 * PI);
+  cairo_fill(cr);
   
-  cairo_destroy(ctx);
-  cairo_surface_destroy(sfc);
+  cairo_destroy(cr);
+  cairo_surface_destroy(sf);
 end;
 
 const
@@ -152,13 +151,13 @@ const
   
 var
   gd, gm, err: smallint;
-  img: PImage;
-  iw, ih, it, il: integer;
-  card: pcairo_surface_t;
+  image: PImage;
+  width, height, left, top: integer;
+  sf_back: pcairo_surface_t;
   ang: double;
   
 begin
-  card := Cardioid(SURFACE_WIDTH, SURFACE_HEIGHT);
+  sf_back := Cardioid(SURFACE_WIDTH, SURFACE_HEIGHT);
   
   WindowTitle := 'A method for drawing a cardioid';
   gd := VESA;
@@ -171,30 +170,30 @@ begin
     SetBkColor($808080);
     ClearDevice;
     
-    iw := SURFACE_WIDTH;
-    ih := SURFACE_HEIGHT;
-    il := (Succ(GetMaxX) - iw) div 2;
-    it := (Succ(GetMaxY) - ih) div 2;
+    width := SURFACE_WIDTH;
+    height := SURFACE_HEIGHT;
+    left := (Succ(GetMaxX) - width) div 2;
+    top := (Succ(GetMaxY) - height) div 2;
     
-    img := CreateImage(iw, ih);
+    image := CreateImage(width, height);
     
     ang := 0;
     while not KeyPressed do
     begin
-      CairoDraw(img^, card, ang);
-      PutImage(il, it, img^, NormalPut);
+      CairoDraw(image^, sf_back, ang);
+      PutImage(left, top, image^, NormalPut);
       
       Delay(60);
       
       ang := ang + PI / 18;
-      if ang > 2 * pi then
-        ang := ang - 2 * pi;
+      if ang > 2 * PI then
+        ang := ang - 2 * PI;
     end;
     
     ReadKey;
     CloseGraph;
-    FreeImage(img, iw, ih);
+    FreeImage(image, width, height);
   end;
   
-  cairo_surface_destroy(card);
+  cairo_surface_destroy(sf_back);
 end.
